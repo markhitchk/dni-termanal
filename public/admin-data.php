5b86c1fc122a0bb90ba6e690a8f6c575c4424573
markhitchk <mark.hitchk@gmail.com>
2026-08-29 02:54:04 -0700

    Fix secure Admin helper bootstrap

--- public/admin-data.php
+++ public/admin-data.php
@@ -2,4 +2,6 @@
 
 declare(strict_types=1);
 
+require_once __DIR__ . '/../server/php/dni-clearance.php';
+require_once __DIR__ . '/admin-operational-helpers.php';
 require __DIR__ . '/admin-secure.php';
