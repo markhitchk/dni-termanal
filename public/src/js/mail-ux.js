345b91887380cb688885f55bd48c71bbf43b7bde
markhitchk <mark.hitchk@gmail.com>
2026-08-31 15:32:00 -0700

    Bust cached DNI Mail composer modules

--- public/src/js/mail-ux.js
+++ public/src/js/mail-ux.js
@@ -1,6 +1,6 @@
-import './mail-address-client.js?v=20260831-mail2';
-import './mail-upload-button.js?v=20260831-mail2';
-import { openMail } from './mail.js?v=20260831-mail2';
+import './mail-address-client.js?v=20260831-mail3';
+import './mail-upload-button.js?v=20260831-mail3';
+import { openMail } from './mail.js?v=20260831-mail3';
 
 const MAIL_URL = '/mail-data.php';
 const DEFAULT_LOGIN_URL = '/auth/discord/login';
