50b4c1815c3c960c55822ea8bcc9c45a3b4b9f27
markhitchk <mark.hitchk@gmail.com>
2026-09-02 12:50:48 -0700

    Expose DNI Admin mail address controller

---
+++ public/admin-mail-address.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Public compatibility controller. Canonical implementation:
+ * server-http/admin-mail-address.php
+ */
+require dirname(__DIR__) . '/server-http/' . basename(__FILE__);
