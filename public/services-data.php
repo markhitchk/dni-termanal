48a30c000c71947c65df1c008afacdb3b49cf5cc
markhitchk <mark.hitchk@gmail.com>
2026-09-01 22:21:57 -0700

    Block Citizen direct access to DNI Services

--- public/services-data.php
+++ public/services-data.php
@@ -8,4 +8,15 @@ declare(strict_types=1);
  * dni_mariadb_secure_service_rows, dni_embedded_require_operational_resource,
  * minimumClearance.
  */
+require_once dirname(__DIR__) . '/server/php/dni.php';
+require_once dirname(__DIR__) . '/server/php/dni-embedded.php';
+require_once dirname(__DIR__) . '/server/php/dni-authz.php';
+
+dni_start_session();
+$citizenDb = dni_embedded_transaction();
+$citizenUser = dni_embedded_current_user($citizenDb);
+if ($citizenUser !== null && dni_is_citizen_user($citizenUser)) {
+    dni_json(403, dni_citizen_restricted_payload('Services system'));
+}
+
 require dirname(__DIR__) . '/server-http/' . basename(__FILE__);
