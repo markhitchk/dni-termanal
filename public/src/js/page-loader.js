66dc513ec05813d30f3c43a4048907a3a9d861e5
markhitchk <mark.hitchk@gmail.com>
2026-09-02 01:53:13 -0700

    Load mobile DNI settings theme after settings controller

--- public/src/js/page-loader.js
+++ public/src/js/page-loader.js
@@ -117,7 +117,9 @@
                 loadModule('src/js/terminal-help-cleanup.js', () => {
                   loadModule('src/js/terminal/terminal-help-layout.js');
                   loadModule('src/js/terminal/terminal-logout.js');
-                  loadModule('src/js/terminal/user-settings.js');
+                  loadModule('src/js/terminal/user-settings.js', () => {
+                    loadModule('src/js/terminal/user-settings-theme.js');
+                  });
                   loadModule('src/js/mail/mail-actions.js');
                 });
               });
