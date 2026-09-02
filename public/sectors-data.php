d5038db6eaef3986e06abbbff39ad6b253ce4770
markhitchk <mark.hitchk@gmail.com>
2026-09-01 22:22:04 -0700

    Block Citizen direct access to DNI Sectors

--- public/sectors-data.php
+++ public/sectors-data.php
@@ -9,4 +9,15 @@ declare(strict_types=1);
  * dni_embedded_require_operational_resource, dni_embedded_new_operational_level,
  * minimumClearance.
  */
+require_once dirname(__DIR__) . '/server/php/dni.php';
+require_once dirname(__DIR__) . '/server/php/dni-embedded.php';
+require_once dirname(__DIR__) . '/server/php/dni-authz.php';
+
+dni_start_session();
+$citizenDb = dni_embedded_transaction();
+$citizenUser = dni_embedded_current_user($citizenDb);
+if ($citizenUser !== null && dni_is_citizen_user($citizenUser)) {
+    dni_json(403, dni_citizen_restricted_payload('Sectors system'));
+}
+
 require dirname(__DIR__) . '/server-http/' . basename(__FILE__);
